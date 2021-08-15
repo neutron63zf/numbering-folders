7a1f99aef8bf23cb8befa1074be14766e907d9b1
Nkowne63 <neutron63zf@gmail.com>
2021-08-15 23:56:54 +0900

    add: parser

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+pub mod args_struct {
+    use structopt::StructOpt;
+
+    #[derive(Debug, StructOpt)]
+    #[structopt(
+        name = "Folders Numbering",
+        about = "Prefix folders with number",
+        author = "Nkowne63"
+    )]
+    pub struct CommandLineData {
+        #[structopt(subcommand)]
+        subcommand: Option<SubCommand>,
+    }
+
+    impl CommandLineData {
+        pub fn get_subcommand(self) -> Option<SubCommand> {
+            return self.subcommand;
+        }
+    }
+
+    #[derive(Debug, StructOpt)]
+    pub enum SubCommand {
+        Order,
+        Number(NumberData),
+    }
+
+    #[derive(Debug, StructOpt)]
+    pub struct NumberData {
+        #[structopt(short = "n", long = "number")]
+        number: i32,
+        #[structopt(short = "f", long = "folder")]
+        folder_name: String,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use fnum::args_struct::{CommandLineData, SubCommand};
+
+#[paw::main]
+fn main(opt: CommandLineData) {
+    match opt.get_subcommand() {
+        Some(SubCommand::Order) => println!("Start ordering..."),
+        Some(SubCommand::Number(number_data)) => println!("number data: {:?}", number_data),
+        None => println!("No subcommand given"),
+    }
 }
